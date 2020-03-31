82d840c4a8ab4cd0a66ee7fbd6a64ff206542ff6
Kazuki Tobita <kazukiti201@gmail.com>
2020-03-31 23:43:37 +0900

    Modify slack

--- cache_musics.py
+++ cache_musics.py
@@ -21,10 +21,14 @@ class CacheMusics:
         return self
 
     def find(self, id = 1):
-        return self.cur.execute('SELECT * FROM musics WHERE id = ?', (str(id),)).fetchone()
+        return self.cur.execute('SELECT * FROM musics WHERE id = ?', (str(id), )).fetchone()
 
     def count(self):
         return self.cur.execute('SELECT count(*) FROM musics').fetchall()[0][0]
+
+    def exists(self, song_name, artist):
+        self.cur.execute('SELECT * FROM musics WHERE EXISTS(SELECT * FROM musics WHERE song_name = ? AND artist = ?)', (song_name, artist, ))
+        return True if self.cur.fetchone() else False
 
     def close(self):
         self.conn.close()
--- scrape.py
+++ scrape.py
@@ -18,7 +18,9 @@ class Scrape:
             song_name = music.find('span', attrs={'class': 'song-name'})
             artist = music.find('span', attrs={'class': 'artist-name'})
             # audio = song.find('span', attrs={'class': 'audition'})
-            self.cache_musics.insert(song_name.contents[0], artist.contents[0])
+            exists = self.cache_musics.exists(song_name.contents[0], artist.contents[0])
+            if (not exists):
+                self.cache_musics.insert(song_name.contents[0], artist.contents[0])
 
 if __name__ == '__main__':
     print('Scraping musics ...')
